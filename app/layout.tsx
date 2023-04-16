6cb6985db2c0f12e285ca869b5070cdd824ea26b
Leandro Arturi <lea.arturi@gmail.com>
2023-04-16 13:27:14 -0300

    Navbar WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
-
 import { Nunito } from 'next/font/google'
+import Nabvar from './components/navbar/Navbar'
 
 export const metadata = {
   title: 'Airbnb Clone',
@@ -18,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Nabvar />
+        {children}
+      </body>
     </html>
   )
 }
