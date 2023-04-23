5f610d212e31758505bb4803a7204f38bc827c24
Leandro Arturi <lea.arturi@gmail.com>
2023-04-23 14:59:59 -0300

    Listing Cards Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-export default function Home() {
-   const isEmpty = true;
+export default async function Home() {
+   const listings = await getListings({});
+   const currentUser = await getCurrentUser();
 
-   if (isEmpty) {
+   if (listings.length === 0) {
       return (
          <ClientOnly>
             <EmptyState showReset />
@@ -18,17 +22,23 @@ export default function Home() {
             <div
                className='
                   pt-24
-                  grid
-                  grid-cols-1
-                  sm:grid-cols-2
-                  md:grid-cols-3
+                  grid 
+                  grid-cols-1 
+                  sm:grid-cols-2 
+                  md:grid-cols-3 
                   lg:grid-cols-4
                   xl:grid-cols-5
                   2xl:grid-cols-6
                   gap-8
             '
             >
-               <div>My Future Listings</div>
+               {listings.map((listing: any) => (
+                  <ListingCard
+                     currentUser={currentUser}
+                     key={listing.id}
+                     data={listing}
+                  />
+               ))}
             </div>
          </Container>
       </ClientOnly>
