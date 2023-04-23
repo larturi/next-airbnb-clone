84957d08539931aea55210de1acc00749817cf0a
Leandro Arturi <lea.arturi@gmail.com>
2023-04-23 14:31:28 -0300

    EmptyState component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+
 export default function Home() {
-  return (
-    <></>
-  )
+   const isEmpty = true;
+
+   if (isEmpty) {
+      return (
+         <ClientOnly>
+            <EmptyState showReset />
+         </ClientOnly>
+      );
+   }
+   return (
+      <ClientOnly>
+         <Container>
+            <div
+               className='
+                  pt-24
+                  grid
+                  grid-cols-1
+                  sm:grid-cols-2
+                  md:grid-cols-3
+                  lg:grid-cols-4
+                  xl:grid-cols-5
+                  2xl:grid-cols-6
+                  gap-8
+            '
+            >
+               <div>My Future Listings</div>
+            </div>
+         </Container>
+      </ClientOnly>
+   );
 }
