806179f3d84f34fd1d43b5e4b2ac379ee06d3239
Leandro Arturi <lea.arturi@gmail.com>
2023-04-25 15:48:28 -0300

    GetReservations and block in Calendar

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,11 +1,13 @@
 import getListingById, { IListingParams } from '../../actions/getListingById';
 import getCurrentUser from '../../actions/getCurrentUser';
+import getReservations from '../../actions/getReservations';
 import ClientOnly from '../../components/ClientOnly';
 import EmptyState from '../../components/EmptyState';
 import ListingClient from './ListingClient';
 
 const ListingPage = async ({ params }: { params: IListingParams }) => {
    const listing = await getListingById(params);
+   const reservations = await getReservations(params);
    const currentUser = await getCurrentUser();
 
    if (!listing) {
@@ -17,7 +19,11 @@ const ListingPage = async ({ params }: { params: IListingParams }) => {
    }
    return (
       <ClientOnly>
-         <ListingClient listing={listing} currentUser={currentUser} />
+         <ListingClient
+            listing={listing}
+            currentUser={currentUser}
+            reservations={reservations}
+         />
       </ClientOnly>
    );
 };
